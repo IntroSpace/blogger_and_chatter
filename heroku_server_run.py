3a9921bf19ca6cfe2692379030c2ca460d0d27c9
IntroSpace <IntroSpace321@gmail.com>
2022-03-23 10:26:15 +0300

    update: fixed problem with heroku server

--- heroku_server_run.py
+++ heroku_server_run.py
@@ -1,7 +1,10 @@
 import os
+
+from data import db_session
 from main import app
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/social_net.db")
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
