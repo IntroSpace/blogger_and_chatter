a77e43b6bc26900dc7cf98d52b550d3412a1e74e
IntroSpace <IntroSpace321@gmail.com>
2022-03-23 16:16:23 +0300

    update: heroku postgresql test

--- heroku_server_run.py
+++ heroku_server_run.py
@@ -5,6 +5,6 @@ from main import app
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/social_net.db")
+    db_session.global_init()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
--- main.py
+++ main.py
@@ -213,5 +213,5 @@ def all_users_list():
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/social_net.db")
+    db_session.global_init()
     app.run(port=8000, host='127.0.0.1')
