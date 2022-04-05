176257b424747cba4a698e210a3baf45f306eaf8
IntroSpace <IntroSpace321@gmail.com>
2022-04-05 19:06:37 +0300

    update: users can create posts and see them

--- main.py
+++ main.py
@@ -60,55 +60,57 @@ def get_all_info(current_pos):
 
 
 def get_all_blogs(extra_blog=None):
-    all_blogs = [
-        {
-            'id': 1,
-            'text': 'post 1',
-            'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-            'visual_content': 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-            'liked': False
-        },
-        {
-            'id': 2,
-            'text': 'post 2',
-            'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149452.png',
-            'liked': True
-        },
-        {
-            'id': 3,
-            'text': 'post 3',
-            'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-            'visual_content': 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80',
-            'liked': False
-        },
-        {
-            'id': 4,
-            'text': 'post 4',
-            'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149995.png',
-            'liked': True
-        },
-        {
-            'id': 5,
-            'text': 'post 5',
-            'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-            'visual_content': 'https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567__480.jpg',
-            'liked': False
-        },
-        {
-            'id': 6,
-            'text': 'post 6',
-            'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149995.png',
-            'liked': True
-        },
-        {
-            'id': 7,
-            'text': 'post 7',
-            'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149995.png',
-            'liked': True
-        }
-    ]
-    if extra_blog is not None:
-        all_blogs = list(filter(lambda x: x.get('id', -1) != extra_blog, all_blogs))
+    # all_blogs = [
+    #     {
+    #         'id': 1,
+    #         'text': 'post 1',
+    #         'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    #         'visual_content': 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    #         'liked': False
+    #     },
+    #     {
+    #         'id': 2,
+    #         'text': 'post 2',
+    #         'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149452.png',
+    #         'liked': True
+    #     },
+    #     {
+    #         'id': 3,
+    #         'text': 'post 3',
+    #         'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    #         'visual_content': 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80',
+    #         'liked': False
+    #     },
+    #     {
+    #         'id': 4,
+    #         'text': 'post 4',
+    #         'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149995.png',
+    #         'liked': True
+    #     },
+    #     {
+    #         'id': 5,
+    #         'text': 'post 5',
+    #         'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    #         'visual_content': 'https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567__480.jpg',
+    #         'liked': False
+    #     },
+    #     {
+    #         'id': 6,
+    #         'text': 'post 6',
+    #         'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149995.png',
+    #         'liked': True
+    #     },
+    #     {
+    #         'id': 7,
+    #         'text': 'post 7',
+    #         'avatar': 'https://cdn-icons-png.flaticon.com/512/149/149995.png',
+    #         'liked': True
+    #     }
+    # ]
+    db_sess = db_session.create_session()
+    all_blogs = db_sess.query(Post).all()
+    # if extra_blog is not None:
+    #     all_blogs = list(filter(lambda x: x.get('id', -1) != extra_blog, all_blogs))
     return all_blogs
 
 
@@ -260,7 +262,8 @@ def new_post():
     db_sess = db_session.create_session()
     form = NewPostForm()
     if form.validate_on_submit():
-        post = Post(text=form.text.data, user_id=current_user.id)
+        post = Post(text=form.text.data,
+                    user_id=current_user.id)
         filename = secure_filename(form.file.data.filename)
         form.file.data.save(os.path.join('static/img/posts', filename))
         post.generate_blob(filename)
