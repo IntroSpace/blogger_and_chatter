8b387c5b53865ee97be9facec5b7c15bff00ff49
Ononim03 <tzinatullin16@gmail.com>
2022-04-16 16:40:02 +0300

    update: changes by ProBook_010

--- forms/new_chat_form.py
+++ forms/new_chat_form.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileField
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class NewChatForm(FlaskForm):
     text = TextAreaField('name', validators=[DataRequired()])
-    user = TextAreaField('user', validators=[FileRequired()])
+    photo = FileField('photo', validators=[FileRequired()])
     submit = SubmitField('add')
-
